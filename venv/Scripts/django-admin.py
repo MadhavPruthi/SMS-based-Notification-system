1b8697f176e2a8c91f7c6a74a280d4cf42fe8f0f
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2018-06-09 20:49:37 +0530

    Removed excess files

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\HP\Desktop\SMS-based-Notification-system-master\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
