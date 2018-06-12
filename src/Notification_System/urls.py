e64844a9130e18612cd3aa5d3dc27a7bdc61cf6b
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2018-06-12 22:45:19 +0530

    Login form created

--- src/Notification_System/urls.py
+++ src/Notification_System/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^form/$', TemplateView.as_view(template_name='accounts/form.html'))
+    url(r'^form/$', TemplateView.as_view(template_name='accounts/form.html')),
+    url(r'^login/$', TemplateView.as_view(template_name='accounts/loginForm.html')),
 ]
---
+++ src/static/js/loginform.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Madhav on 12-06-2018.
+ */
+
+$(document).ready(function() {
+        M.updateTextFields();
+});
+
+$(document).ready(function(){
+    $("#fliptofaculty").click(function(){
+        $("#main1").css("-webkit-animation-name","showfaculty");
+        $("#main1").css("transform","translate(-50%,-50%) rotateY(180deg)");
+
+        $("#main2").css("-webkit-animation-name","hidestudent");
+        $("#main2").css("transform","translate(-50%,-50%) rotateY(0deg)");
+
+        $("#form1").css("-webkit-animation-name","showfaculty");
+        $("#form1").css("transform","translate(-50%,-50%) rotateY(180deg)");
+
+        $("#form2").css("-webkit-animation-name","hidestudent");
+        $("#form2").css("transform","translate(-50%,-50%) rotateY(0deg)");
+    });
+
+
+    $("#fliptostudent").click(function(){
+        $("#form1").css("-webkit-animation-name","hidefaculty");
+        $("#form1").css("transform","translate(-50%,-50%) rotateY(0deg)");
+
+        $("#form2").css("-webkit-animation-name","showstudent");
+        $("#form2").css("transform","translate(-50%,-50%) rotateY(180deg)");
+
+        $("#main1").css("-webkit-animation-name","hidefaculty");
+        $("#main1").css("transform","translate(-50%,-50%) rotateY(0deg)");
+
+        $("#main2").css("-webkit-animation-name","showstudent");
+        $("#main2").css("transform","translate(-50%,-50%) rotateY(180deg)");
+    });
+});
+
+
+
+
+
+
+
