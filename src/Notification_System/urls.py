30360b045bc3aa58d84abe018ffbc93a2b0d799f
Shreya <shreyagrg09@gmail.com>
2018-06-18 05:07:49 -0700

    contct_us

--- src/Notification_System/urls.py
+++ src/Notification_System/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
     url(r'^form/$', TemplateView.as_view(template_name='accounts/form.html')),
+    url(r'^form/signup$', TemplateView.as_view(template_name='accounts/form.html')),
     url(r'^login/$', TemplateView.as_view(template_name='accounts/loginForm.html')),
     url(r'^signup/$', TemplateView.as_view(template_name='accounts/signupForm.html')),
+url(r'^contact/$', TemplateView.as_view(template_name='contact/contact_us.html')),
 
 ]
