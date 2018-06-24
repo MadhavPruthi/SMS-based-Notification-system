de9fdb351e114a1e283a6b9eae0da2de43fdaff7
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2018-06-24 21:29:52 +0530

    New

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!f:\projects\notification_system\notification\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- src/Notification/models.py
+++
@@ -1,9 +0,0 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import models
-from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
-from .models import  models
-
-class Notif(models.model):
-    NotifId = models.IntegerField(max_length=8,blank=False,unique=True)
-    Notifmessage=models.CharField(max_length=200,blank=False)
